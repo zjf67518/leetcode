a1d8ed6c9f0d50aeeca16b7d3daecb1d7ecd180c
kevenzhu <1091639781@qq.com>
2021-09-01 15:43:26 +0800

    123

--- src/DeadLock.java
+++ src/DeadLock.java
@@ -1,38 +1,33 @@
-import java.util.LinkedList;
 
 public class DeadLock {
-    public static final String obj1 = "obj1";
-    public static final String obj2 = "obj2";
+    public static final DeadLock obj1 = new DeadLock();
+    public static final DeadLock obj2 = new DeadLock();
     public static void main(String[] args){
         new Thread(() -> {
-                while(true) {
-                    synchronized (DeadLock.obj1){
-                        System.out.println("get lock " + obj1);
-                        try {
-                            Thread.sleep(3000);
-                            synchronized (DeadLock.obj2){
-                                System.out.println("get lock " + obj2);
-                            }
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            synchronized (obj1){
+                System.out.println("get lock obj1");
+                try {
+                    Thread.sleep(3000);
+                    synchronized (obj2){
+                        System.out.println("get lock obj2");
                     }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+            }
             }).start();
         new Thread(() -> {
-                while(true) {
-                    synchronized (DeadLock.obj2){
-                        System.out.println("get lock " + obj2);
-                        try {
-                            Thread.sleep(3000);
-                            synchronized (DeadLock.obj1){
-                                System.out.println("get lock " + obj1);
-                            }
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            synchronized (obj2){
+                System.out.println("get lock obj2");
+                try {
+                    Thread.sleep(3000);
+                    synchronized (obj1){
+                        System.out.println("get lock obj1");
                     }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+            }
            }).start();
     }
 }
